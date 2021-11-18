858109e8417ef7be0d20305d6d490cd56543d016
Francisco <jhersonhuacho@gmail.com>
2021-11-18 11:34:42 -0500

    add heroku needed config

--- part3-Midudev/index.js
+++ part3-Midudev/index.js
@@ -40,7 +40,7 @@ let notes = [
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 const logger = require('./loggerMiddleware')
 
 app.use(cors()) // por defecto cualquier origen pueda conectarse a nuestra api
